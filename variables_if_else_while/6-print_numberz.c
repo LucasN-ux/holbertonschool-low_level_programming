d990b2d673a88e2d7629e7821d97a22738fe1ff4
LucasN-ux <lucas.n230896@gmail.com>
2025-10-21 18:01:12 +0000

    feat:prints the digits 0 to 9 whithout char

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the digits 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+
+	putchar('\n');
+	return (0);
+}
