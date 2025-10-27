9600c6ec8d0241860741147a442184a3c46016dd
LucasN-ux <lucas.n230896@gmail.com>
2025-10-27 10:32:20 +0000

    feat: Write a function that multiplies two integers(Prototype: int mul(int a, int b))

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: caracter to check
+ * @b: caracter to check
+ * Return: result a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
