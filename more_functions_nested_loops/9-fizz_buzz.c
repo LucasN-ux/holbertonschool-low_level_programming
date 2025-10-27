b6f44568c71f96be4909afee6edcde6efded8388
LucasN-ux <lucas.n230896@gmail.com>
2025-10-27 14:40:24 +0000

    feat: FizzBuzz program 1 to 100

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100, with fizzBuzz multiples
+ * Return: int
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (n % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+			printf("%d", n);
+		putchar(' ');
+	}
+	return (0);
+}
