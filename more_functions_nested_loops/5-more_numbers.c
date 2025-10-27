52539d08c249a7fcd751b4a91f0b57872cea94c7
LucasN-ux <lucas.n230896@gmail.com>
2025-10-27 12:30:26 +0000

    fix: prints 10 times the numbers, from 0 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,16 @@ void more_numbers(void)
 	int n;
 	int line;
 
-	for (line = 0; line <= 14; line++)
+	for (line = 0; line <= 10; line++)
 	{
-		for (n = '0'; n <= '9'; n++)
+		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n);
+			if (n >= 10)
+			{
+				_putchar('1');
+			}
+			
+			_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
 	}
