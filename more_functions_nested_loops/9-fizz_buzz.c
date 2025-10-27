e18817153a33f9b5081c6317c2cf3124c0aace2a
LucasN-ux <lucas.n230896@gmail.com>
2025-10-27 14:49:44 +0000

    fix: order condition FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,11 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
 		{
 			printf("Fizz");
 		}
@@ -19,13 +23,10 @@ int main(void)
 		{
 			printf("Buzz");
 		}
-		else if (n % 15 == 0)
-		{
-			printf("FizzBuzz");
-		}
 		else
 			printf("%d", n);
-		putchar(' ');
+		if (n != 100)
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
