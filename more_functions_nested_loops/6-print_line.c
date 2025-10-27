5d8edd207848b6ae4e0a2ae1b094e317f19a84af
LucasN-ux <lucas.n230896@gmail.com>
2025-10-27 13:06:10 +0000

    feat: Write a function that draws a straight line in the terminal.(Prototype: void print_line(int n);)

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - draws a straight line in the terminal.
+ * @n: caracter to check
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int result = 0;
+
+	while (result < n)
+	{
+		_putchar('_');
+		result++;
+	}
+	_putchar('\n');
+}
