d78eddf205c581cbbf849942c1446f6e47e357c9
LucasN-ux <lucas.n230896@gmail.com>
2025-10-29 13:54:51 +0000

    Write a function that concatenates two strings (Prototype: char *_strcat(char *dest, char *src))

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat -  concatenates two strings
+ * @dest: point to the entier modified
+ * @src: point to the entier modified
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
