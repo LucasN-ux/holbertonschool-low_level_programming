fe967ebb468f1be520c24d2bf658ecaced197ffd
LucasN-ux <lucas.n230896@gmail.com>
2025-10-31 21:56:32 +0100

    fix: int variable started to print the long numbers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,23 +11,24 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int ent = 0;
+	int started = 0;
 
-	while (s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'))
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 		{
-			sign = -sign;
+			sign *= -1;
 		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			ent = ent * 10 + (s[i] - '0');
+			started = 1;
+		}
+		else if (started)
+			break;
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (ent == -1)
-			ent = ent * 10 - (s[i] - '0');
-		else
-			ent = ent * 10 + (s[i] - '0');
-		i++;
-	}
 	return (ent * sign);
 }
