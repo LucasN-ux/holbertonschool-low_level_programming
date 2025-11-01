e91deb58d7479b9d3d098a1ec7439d32d3df8d97
LucasN <lucas.n230896@gmail.com>
2025-11-01 14:32:36 +0000

    feat: Write a function that encodes a string with rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into 1337
+ * @char: string to encode
+ *
+ * Return: pointer to str
+ */
+char *rot13(char *)
+{
+
+	char in[] = "aAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char out[] = "nNoOpPqQrRsStTuUvVwWxXyYzZ";
+
+
+
+}
