af61f7d3f6a37960053ac9e39b18518554d08080
LucasN <lucas.n230896@gmail.com>
2025-11-01 15:28:37 +0000

    fix: Write a function that encodes a string using rot13.

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,17 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string into 1337
- * @char: string to encode
+ * rot13 - Write a function that encodes a string
+ * @str: string to encode
  *
  * Return: pointer to str
  */
-char *rot13(char *)
+char *rot13(char *str)
 {
+	int i = 0;
 
-	char in[] = "aAbBcCdDeEfFgGhHiIjJkKlLmM";
-	char out[] = "nNoOpPqQrRsStTuUvVwWxXyYzZ";
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = ((str[i] - 'a' + 13) % 26) + 'a';
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = ((str[i] - 'A' + 13) % 26) + 'A';
+		}
+		i++;
+	}
 
-
-
+	return (str);
 }
