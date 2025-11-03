8ee73e5c982c233e6a8437823d07ec6ba8a03ba7
LucasN <lucas.n230896@gmail.com>
2025-11-03 09:58:52 +0000

    feat: Write a function that fills memory with a constant byte.

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * s*_memset -  fills memory with a constant byte.
+ * @s: buffer to return
+ * @b: number to print
+ * @n: how many number
+ *
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
