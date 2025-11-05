f69e8bc9dc8bb54ea2331ffbc61d2fd52b212441
LucasN <lucas.n230896@gmail.com>
2025-11-05 15:07:58 +0000

    fix: betty style description

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,12 +3,13 @@
 
 /**
  * main - prints its name, followed by a new line.
- *
+ * @argc: name of program
+ * @argv: array contain name of program
  * Return: int.
  */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return(0);
+	return (0);
 }
