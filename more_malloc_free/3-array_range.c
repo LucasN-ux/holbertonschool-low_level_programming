86df41be61c98c11ec1a32543c95596ce390170a
LucasN <lucas.n230896@gmail.com>
2025-11-12 14:50:52 +0000

    fix :Write a function that creates an array of integers.

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,15 +12,15 @@
 int *array_range(int min, int max)
 {
 	int *ar;
-	int i;
-	int total;
+	unsigned int i;
+	unsigned int total;
 
 	if (min == 0 && max == 0)
 	{
 		return (NULL);
 	}
 
-	total = max - min;
+	total = max - min + 1;
 	ar = malloc(sizeof(int) * (total));
 	if (!ar)
 		return (NULL);
