a23a9eb64121c635fd4b5016e1ae97c8ca82bbf3
LucasN <lucas.n230896@gmail.com>
2025-11-19 15:41:39 +0000

    feat: add the main to prints numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - sum of all paramaters
+ * @separator: number arguments check
+ * @n: number arguments check
+ * Return: sum
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	int i;
+
+}
