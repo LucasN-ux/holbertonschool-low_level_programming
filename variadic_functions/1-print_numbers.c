a9348564b774b991b5760ea4fd94631cce446d2b
LucasN <lucas.n230896@gmail.com>
2025-11-21 09:16:23 +0000

    fix:Write a function that prints numbers, followed by a new line.

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,15 +1,28 @@
 #include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_numbers - sum of all paramaters
- * @separator: number arguments check
+ * print_numbers - Write a function that prints number
+ * @separator: separator numbers
  * @n: number arguments check
- * Return: sum
+ * Return: numbers separate by ,
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	int i;
+	unsigned int i;
+	int num;
 
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(args, int);
+		printf("%d", num);
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
 }
