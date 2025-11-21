da874576442692ca490949b393238b526dab1a98
LucasN <lucas.n230896@gmail.com>
2025-11-21 09:49:51 +0000

    fix: If one of the string is NULL, print (nil) instead

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,7 +18,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		printf("%s", str);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
