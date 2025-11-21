1ad34053f1447b2875f6751b8e266f64b006ae3b
LucasN <lucas.n230896@gmail.com>
2025-11-21 15:01:49 +0000

    fix:Write a function that prints anything.

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef _MAIN_
 #define _MAIN_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
  * struct s_format - structure of format of input
  * @specifier: %d, %s, %c, %f
@@ -8,18 +12,18 @@
  * Return: Always 0.
  */
 
-typedef struct s_format
+typedef struct formats
 {
-	char specifier;
-	void (*func)(va_list args);
+	char *type;
+	void (*func)(va_list);
 
-} t_format;
+} check;
 
 void print_char(va_list args);
 void print_int(va_list args);
 void print_float(va_list args);
 void print_string(va_list args);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
