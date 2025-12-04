f5076a7c4b22874b41eaca3056f75ccc29c90612
LucasN <lucas.n230896@gmail.com>
2025-12-04 17:30:02 +0000

    feat:Write a program that copies the content of a file to another file.

---
+++ file_io/3-cp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - copies the content of a file to another file.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+}
