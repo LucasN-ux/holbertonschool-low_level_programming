024d2906238207238f0b7ec46c944f77199bb9fe
LucasN <lucas.n230896@gmail.com>
2025-12-04 09:38:46 +0000

    fix: betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it
  * @filename: pointer to the name of the file.
  * @letters: number of letters it should read and print.
- * Return: the actual number of letters it could read and print.
+ * Return: write
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
